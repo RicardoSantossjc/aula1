ed94bfa517832bb4a0ad0237b4a770d089f0599f
RicardoSantossjc <ricardo16meister@gmail.com>
2023-12-30 00:48:48 -0300

    Add files via upload
    Meu primeiro projeto

---
+++ ex000.py
@@ -0,0 +1,5 @@
+nome = (input('Ola boa noite, sou o  assistente do site, qual o seu nome?'))
+num1 = int(input('É um grande prazer em recebê-lo aqui, seja bem vindo..., vamos fazer uma soma?.. Digite um numero:'))
+num2 = int(input('Muito bem, agora digite outro numero..'))
+soma = num1+num2
+print ('O resultado é:', soma)
